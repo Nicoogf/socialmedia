e23f3357ea134ca23ddb579ca4764a69e1bc06d2
Nicoogf <nicolasgfalabella@gmail.com>
2023-12-07 13:31:03 -0300

    Fix imagen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react' ;
+
+import { Routes , Route } from 'react-router-dom' ;
+
+import SigninForm from './_auth/forms/SigninForm' ;
+import SignupForm from './_auth/forms/SignupForm' ;
+import Home from './_root/pages/Home' ;
+
+
+import './globals.css' ;
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
-    <div>App nazi</div>
+   <main className='flex h-screen'>
+    <Routes>
+
+      {/* Rutas Publicas */}
+      <Route element={ <AuthLayout /> }>
+        <Route path="sign-in" element={ <SigninForm /> } />
+        <Route path="sign-up" element={ <SignupForm /> } />      
+      </Route> 
+     
+
+       {/* Rutas Privadas */}
+       <Route element= { <RootLayout /> }>
+         <Route index element={ <Home /> }/> 
+       </Route>
+      
+
+
+    </Routes>
+
+   </main>
   )
 }
 
---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const SigninForm = () => {
+  return (
+    <div>
+      
+      <Button>Click me</Button>
+    
+    </div>
+  )
+}
+
+export default SigninForm
